02f07605dc04d81a1f6f91e3356161022eb19b79
alherz <andrew_lee_herz@hotmail.com>
2017-06-07 07:57:16 -0600

    Add files via upload

--- Guessing Game/main.js
+++ Guessing Game/main.js
@@ -2,12 +2,6 @@
  * Created by Andrew on 6/6/2017.
  */
 
-// Function will clear the text from the text field
-function clearTextBoxText() {
-    guessTextBox.value = "";
-    console.log("clear fired");
-}
-
 /* Onclick event that fires it's function when user clicks on the document. It's used to add filler text in the text
 field if the user doesn't enter anything into the field.*/
 document.addEventListener("click", function (event) {
@@ -26,18 +20,35 @@ function randomNumber() {
     console.log(numberToGuess);
 }
 
+// Function will clear the text from the text field
+function clearTextBoxText() {
+    guessTextBox.value = "";
+    console.log("clear fired");
+}
+
 // Checks if player won the game and displays the results
 function didPlayerWin() {
+    // Need to add if to check if player won a game and clear things if so
+
     if(parseInt(guessTextBox.value)){
         var p = document.getElementById("playerInstruction");
-        p.style.color = "black";
         var playerGuess = parseInt(guessTextBox.value);
-        console.log(playerGuess);
+        p.style.color = "black";
+        guessCount += 1;
+        document.getElementById("guessCount").value = guessCount;
         if(playerGuess === numberToGuess)
         {
-            p.innerHTML = "Congrats! You won!";
+            p.innerHTML = "Congrats, You won! Play again?";
+            gamesWon += 1;
+            document.getElementById("playerWins").value = gamesWon;
+            document.getElementById("playerAccuracy").value = (100 - guessCount) + "%";
+            randomNumber();
         } else {
-            p.innerHTML = "Sorry! Try again.";
+            if(playerGuess > numberToGuess){
+                p.innerHTML = "Too High!";
+            } else {
+                p.innerHTML = "Too Low!";
+            }
         }
 
     }else {
@@ -49,8 +60,11 @@ function didPlayerWin() {
 
 // Creates variables and assigns event listeners. Also calls the function to generate a number to guess.
 var numberToGuess;
-randomNumber();
+var gamesWon = 0;
+var guessCount = 0;
+var playerAccuracy = 0;
 var guessTextBox = document.getElementById("numberGuess");
 var gameSubmitButton = document.getElementById("submitButton");
 guessTextBox.addEventListener("click", clearTextBoxText, false);
 gameSubmitButton.addEventListener("click", didPlayerWin, false);
+randomNumber();
